4b038085622b5a683b6b22a5acc50e30f046da46
yifanli <liyifan2019@gmail.com>
2023-03-25 00:56:36 -0500

    style(css):optimize card arrangement using Bootstrap5

--- src/apis/index.php
+++ src/apis/index.php
@@ -2,87 +2,45 @@
 
 /*******
  *
- * This is the main page for the blog
- * It will display the most recent 10 posts
+ * This is the main page for the website
  ****************/
 
-require('connect.php');
-
-// Get the 10 most recent posts
-$query = "SELECT a.*, u.username as author, c.name as category, t.name as tag
-    FROM Articles a
-    LEFT JOIN Users u ON a.user_id = u.id
-    LEFT JOIN Categories c ON a.category_id = c.id
-    LEFT JOIN ArticleTags at ON a.id = at.article_id
-    LEFT JOIN Tags t ON at.tag_id = t.id
-    ORDER BY a.created_at DESC
-    LIMIT 10";
-
-$statement = $db->prepare($query);
-$statement->execute();
-$blogs = $statement->fetchAll();
-
+require(__DIR__ . '/connect.php');
 ?>
 
 
 <!DOCTYPE html>
 <html>
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Women in Web3 Blog</title>
+  <title>Web3 Launchpad</title>
   <!-- CSS Stylesheets -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-lcNNHT73ZFX53eShLrTfU8q6UEsri6CQj6tgmbY+9XhXx39JKTBBfLNYHl09E0hW" crossorigin="anonymous" />
   <link rel="stylesheet" href="../../public/css/style.css">
 </head>
 
 <body>
   <!-- Header -->
-  <?php include 'header.php'; ?>
+  <?php include(__DIR__ . '/header.php'); ?>
   <!-- End of Header -->
 
-
   <!-- Main Content -->
-  <div class="container">
-    <h1 class="text-center">Welcome to Women in Web3</h1>
-    <p class="text-center">Stay updated with the latest news and trends in the world of Web3 and blockchain technology.</p>
-    <div class="row">
+  <div class="container-fluid">
+    <h1 class="text-center mx-auto">Welcome to Web3 Launchpad</h1>
+    <p class="text-center mx-auto">Web3 Playground is a vibrant platform where developers can delve into the exciting world of Web3 and blockchain. Whether you're just starting out or a seasoned pro, our community is here to support your journey.</p>
+    <hr>
+    <?php include(__DIR__ . '/blog-list.php'); ?>
+  </div>
+  <!-- End of Main Content -->
 
-      <!-- Blog Post -->
-        <div id="all_blogs">
-            <?php foreach ($blogs as $blog) : ?>
-              <div class="blog_post">
-                <h2>
-                  <a href="view.php?id=<?= htmlspecialchars($blog['id']) ?>">
-                    <?= htmlspecialchars($blog['title']) ?></a>
-                </h2>
-                <p>
-                  <small>
-                    Category: <?= htmlspecialchars($blog['category']) ?> - Author: <?= htmlspecialchars($blog['author']) ?> - Tags:
-                    <?= htmlspecialchars($blog['tag']) ?>
-                    - Date: <?= date("F d,Y,h:i a", strtotime($blog['created_at'])) ?>
-                  </small>
-                </p>
-                <div class="blog_content">
-                  <?php
-                  $content = htmlspecialchars($blog['content']);
-                  if (strlen($content) > 200) :
-                    $truncated_content = substr($content, 0, 200) . "...";
-                  ?>
-                    <p><?= $truncated_content ?></p>
-                    <a href="view.php?id=<?= htmlspecialchars($blog['id']); ?>">Read Full Blog</a>
-                  <?php else : ?>
-                    <p><?= $content ?></p>
-                  <?php endif ?>
-                </div>
-              </div>
-            <?php endforeach ?>
-        </div>
-      <!-- End of Blog Post -->
+  <!-- Footer -->
+  <?php include 'footer.php'; ?>
+  <!-- End of Footer -->
 
-      <!-- Footer -->
-      <?php include 'footer.php'; ?>
-      <!-- End of Footer -->
+  <!-- JS Scripts -->
+  <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-i/wG9XJlKZ+lrV7nEJf5cQ8jw5y5lBdYJMSmqWj9XshvZuznjqQaS/y+ImWryM8n" crossorigin="anonymous"></script>
+</body>
 </html>
