5b588ab5805611c7d68f9e8136669b55e73d5ee8
yifanli <liyifan2019@gmail.com>
2023-03-27 14:26:43 -0500

    chore:Remove .DS_Store from Git history

--- src/apis/edit.php
+++ src/apis/edit.php
@@ -133,7 +133,7 @@ if (isset($_POST['delete_post'])){
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Women in Web3 Blog</title>
+  <title>Web3 Launchpad</title>
   <!-- CSS Stylesheets -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <link rel="stylesheet" href="../../public/css/style.css">
