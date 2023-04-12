1de9c1c231c04684f259fceddfe2ccfe1092a2c1
yifanli <liyifan2019@gmail.com>
2023-04-11 20:48:13 -0500

    docs: move website elements into includes file

---
+++ app/includes/header.php
@@ -0,0 +1,83 @@
+<!-- 
+  This is the header for the blog 
+  It is included in all pages 
+-->
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- SEO  -->
+  <meta name="description" content="Web3 Launchpad is a one-stop destination for anyone looking to learn and work in the Web3 and blockchain industry.">
+  <meta name="keywords" content="Web3, Blockchain, Dev, Learning, Tutorials">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Web3 Launchpad</title>
+  <!-- CSS Stylesheets -->
+  <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../public/css/bootstrap.css">
+  <link rel="stylesheet" href="../public/css/style.css">
+  <script src="../public/js/post.js"></script>
+
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-ai3DlHwZ5K5W5GTHYYXcBbXEikRyvCtWXBx8Hg+PpDEYw+ZRMHnZa8nXQ2viM59JScfzsFbSVn71MZ0kgQGbQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+
+<body>
+<!-- Header -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Web3 Launchpad</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="true" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse show" id="navbarColor01" style="">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.php">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Explore</a> 
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Learn</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Jobs</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="index.php" role="button" aria-haspopup="true" aria-expanded="false">Tech Blog</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Courses</a>
+            <a class="dropdown-item" href="#">Tutorials</a>
+            <a class="dropdown-item" href="#">TechBlogs</a>
+          </div>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="search" placeholder="Search">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+      <!-- register and login -->
+        <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="register.php">Register</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- End of Header -->
+
+
+
+
+
+
