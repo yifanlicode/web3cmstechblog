ce0a9c0ecd661ad59df5163aaea9a1705e49f2f3
yifanli <liyifan2019@gmail.com>
2023-04-17 18:59:14 -0500

    fix:fix the sort button

--- app/index.php
+++ app/index.php
@@ -6,14 +6,15 @@
  * home page
  ****************/
 
+if (!isset($_SESSION)) {
+  session_start();
+}
+
 require 'includes/connect.php';
-
 $sort = isset($_GET['sort']) ? $_GET['sort'] : '';
-
 include('includes/header.php');
 
 ?>
-
 
 <!-- Main Content -->
 <div class="container-fluid my-5">
@@ -25,23 +26,25 @@ include('includes/header.php');
     </div>
   </div>
 
-
   <div class="row">
     <div class="col-lg-9">
       <!-- Sort by dropdown menu -->
-      <div class="d-flex justify-content-end mb-3">
-        <label class="me-2" for="sort-by-select">Sort by:</label>
-        <select id="sort-by-select" onchange="sortPosts(this.value)">
-          <option value="post_views_count" <?php if ($sort == '' || $sort == 'post_views_count') echo 'selected' ?>>Views</option>
-          <option value="post_date" <?php if ($sort == 'post_date') echo 'selected' ?>>Post Date</option>
-          <option value="update_date" <?php if ($sort == 'update_date') echo 'selected' ?>>Update Date</option>
-        </select>
-      </div>
+      <?php if (isset($_SESSION['user_id'])) : ?>
+        <div class="d-flex justify-content-end mb-3 align-items-center">
+          <label class="me-2 fw-bold" for="sort-by-select">Sort by:</label>
+          <select class="form-select-sm rounded-pill border-0" onchange="sortPosts(this.value)">
+            <option value="post_views_count" <?php if ($sort == '' || $sort == 'post_views_count') echo 'selected' ?>>Views</option>
+            <option value="post_title" <?php if ($sort == 'post_title') echo 'selected' ?>>Title</option>
+            <option value="post_date" <?php if ($sort == 'post_date') echo 'selected' ?>>Post Date</option>
+            <option value="update_date" <?php if ($sort == 'update_date') echo 'selected' ?>>Update Date</option>
+          </select>
+        </div>
+      <?php endif; ?>
 
-
+    
       <!-- Include blog-list.php -->
       <div id="blog-list-container">
-        <?php include 'blog-list.php'; ?>
+        <?php include 'includes/blog-list.php'; ?>
       </div>
     </div>
 
